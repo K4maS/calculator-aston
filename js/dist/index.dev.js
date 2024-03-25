5e7fe77dffbd404d0a57b70089bd9b4c45413e51
k4mas <sultanowkamilgod@gmail.com>
2024-03-25 16:20:55 +0500

    Почти сделано, с классами

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -7,9 +7,16 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 (function () {
+  'use struct';
+
+  var keys = ['ac', '⇐', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+  var value = '';
   var value1 = '';
   var value2 = '';
+  var value1IsEmpty = true;
   var operator = '';
+  var result = 0;
+  var lastRender = false; // Класс вычисления
 
   var Calculator =
   /*#__PURE__*/
@@ -56,8 +63,8 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
         return this.x / this.y;
       }
     }, {
-      key: "logDiv",
-      value: function logDiv() {
+      key: "logDivCirc",
+      value: function logDivCirc() {
         if (this.y === 0) {
           throw new Error('second operand is 0');
         }
@@ -67,9 +74,10 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     }]);
 
     return Calculator;
-  }();
+  }(); // Создал экземпляр калсса
 
-  var calcEx = new Calculator(0, 0);
+
+  var calculatorExemplar = new Calculator(0, 0); // Проверка на число
 
   function numCheck(num) {
     num = Number(num);
@@ -79,36 +87,36 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     }
 
     return num;
-  }
+  } // Рисует корпус калькулятора
 
-  function drawCalcFrame(display, keyBoard) {
+
+  function drawCalcFrame(keyBoard, display) {
     var frame = document.createElement('div');
     frame.classList.add('calc__frame');
     frame.append(display, keyBoard);
     return frame;
-  }
+  } // Рисует дисплей, принимает текст
+
 
   function drawDisplay(value) {
     var display = document.createElement('div');
     display.classList.add('calc__display');
     display.textContent = value;
     return display;
-  }
+  } // Рисует кнопку, принимпет символ
 
-  function drawCalcBtn(value) {
+
+  function drawCalcBtn(btnValue) {
     var button = document.createElement('button');
     button.classList.add('calc__button');
-    button.textContent = value;
-
-    if (value === 'ac') {
-      calcEx.setX = 0;
-      calcEx.setY = 0;
-    }
-
-    if (value === '<=') {} else if (value === '%') {} else if (value === '/') {} else if (value === 'x') {} else if (value === '-') {} else if (value === '+') {} else if (value === '.') {} else if (value === '=') {} else {}
-
+    button.textContent = btnValue;
+    button.addEventListener('click', function () {
+      console.log(btnValue);
+      doClick(btnValue);
+    });
     return button;
-  }
+  } // По саписку символов рисует клавитатуру
+
 
   function drawKeyboard(keys) {
     var keyboard = document.createElement('div');
@@ -139,16 +147,139 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     }
 
     return keyboard;
+  } // При нажатии на кнопку
+
+
+  function doClick(btnValue) {
+    var operandsList = ['%', '/', 'x', '+'];
+
+    if (btnValue === 'ac') {
+      calculatorExemplar.setX = 0;
+      calculatorExemplar.setY = 0;
+      value1 = '';
+      value2 = '';
+      value = '';
+      result = 0;
+      operator = '';
+      value1IsEmpty = true;
+    } else if (btnValue === '⇐') {
+      if (result) {
+        result = String(result).slice(0, -1);
+      }
+
+      if (result) {
+        result = String(result).slice(0, -1);
+      }
+    } // else if('%/x+'.includes(btnValue)) {
+    //     OperatorAction()
+    // }
+    // else if(btnValue === '%') {
+    //     OperatorAction()
+    // } else if(btnValue === '/') {
+    //     OperatorAction()
+    // } else if(btnValue === 'x') {
+    //     OperatorAction()
+    // } else if(btnValue === '+') {
+    //     OperatorAction()
+    // }
+    else if (isNaN(btnValue) && operandsList.includes(btnValue)) {
+        getRepeatResult(btnValue);
+      } else if (btnValue === '-') {
+        if (value1 === '') {
+          value = btnValue + value;
+        }
+
+        if (operator && value2 === '') {
+          value = btnValue + value;
+        } else {
+          getRepeatResult(btnValue);
+        }
+      } else if (btnValue === '.') {
+        value = value + btnValue;
+      } else if (btnValue === '=') {
+        getRepeatResult('', value);
+      } else {
+        value = value + btnValue;
+      }
+
+    if (value1IsEmpty) {
+      value1 = value;
+    } else {
+      value2 = value;
+    }
+
+    console.log(value1);
+    console.log(operator);
+    console.log(value2);
+
+    if (lastRender) {
+      Render("".concat(value1).concat(operator).concat(value2, "=").concat(result));
+      lastRender = false;
+    } else {
+      if (value1 === '' && value2 === '') {
+        Render(result);
+      } else {
+        Render("".concat(value1).concat(operator).concat(value2));
+      }
+    }
+  } // результат мат операции
+
+
+  function getResult() {
+    try {
+      console.log('va1', value1, 'va2', value2);
+      calculatorExemplar.setX(Number(value1));
+      calculatorExemplar.setY(Number(value2));
+
+      if (operator === '+') {
+        result = calculatorExemplar.logSum();
+      }
+
+      if (operator === '-') {
+        result = calculatorExemplar.logSub();
+      }
+
+      if (operator === '/') {
+        result = calculatorExemplar.logDiv();
+      }
+
+      if (operator === '%') {
+        result = calculatorExemplar.logDivCirc();
+      }
+
+      if (operator === 'x') {
+        result = calculatorExemplar.logMul();
+      }
+
+      lastRender = true;
+    } catch (err) {
+      Render('err');
+    }
   }
 
-  function doCalculation(x, y, operator) {}
+  function getRepeatResult(btnValue) {
+    var val = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
 
-  window.addEventListener('DOMContentLoaded', function () {
-    var keys = ['ac', '<=', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
-    var app = document.getElementById('app');
-    var display = drawDisplay(0);
+    if (value1 && value2) {
+      getResult();
+      value1 = result;
+      value2 = '';
+      lastRender = false;
+    }
+
+    value1IsEmpty = false;
+    operator = btnValue;
+    value = val;
+  } // Отрисовка калькулятора
+
+
+  var Render = function Render(displayText) {
     var keyboard = drawKeyboard(keys);
-    var calcFrame = drawCalcFrame(display, keyboard);
+    app.innerHTML = '';
+    var display = drawDisplay(displayText);
+    var calcFrame = drawCalcFrame(keyboard, display);
     app.append(calcFrame);
-  });
+  };
+
+  Render(0);
 })();
--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 (()=>{
 
+    'use struct'
+
+    const keys = ['ac', '⇐', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2,3 ,'+', 0, '.', '='];
+
+    let value = '';
     let value1 = '';
     let value2 = '';
+
+    let value1IsEmpty = true;
     let operator = '';
 
+    let result = 0;
+    let lastRender = false;
+
+    // Класс вычисления
     class Calculator {
         constructor (x, y) {
             this.x = x;
@@ -38,7 +49,7 @@
             }
             return this.x / this.y;
         }
-        logDiv() {
+        logDivCirc() {
             if(this.y === 0) {
                 throw new Error('second operand is 0');
             }
@@ -46,87 +57,236 @@
         }
     }
     
-    const calcEx = new Calculator (0, 0)
+    // Создал экземпляр калсса
+    const calculatorExemplar = new Calculator (0, 0)
 
+    // Проверка на число
     function numCheck(num) {
+        
         num = Number(num);
+        
         if(isNaN(num)) {
             throw new Error('Value is not a number');
         }
+
         return num;
+
     }
     
-
-    function drawCalcFrame (display, keyBoard) {
+    // Рисует корпус калькулятора
+    function drawCalcFrame (keyBoard, display) {
+        
         const frame =  document.createElement('div');
+        
         frame.classList.add('calc__frame');
         frame.append(display, keyBoard);
+       
         return frame;
+
     }
 
+    // Рисует дисплей, принимает текст
     function drawDisplay (value) {
+        
         const display =  document.createElement('div');
+        
         display.classList.add('calc__display');
         display.textContent = value;
+        
         return display;
+
     }
 
-    function drawCalcBtn (value) {
-
-        const button =  document.createElement('button');
-        button.classList.add('calc__button');
-        button.textContent = value;
-
-        if(value === 'ac') {
-            calcEx.setX = 0;
-            calcEx.setY = 0;
-        }
-        if(value === '<=') {
-
-        } else if(value === '%') {
-
-        } else if(value === '/') {
-
-        } else if(value === 'x') {
-
-        } else if(value === '-') {
-
-        } else if(value === '+') {
-
-        } else if(value === '.') {
-
-        } else if(value === '=') {
-
-        } else {
-            
-        }
+    // Рисует кнопку, принимпет символ
+    function drawCalcBtn (btnValue) {
         
+        const button =  document.createElement('button');
+        
+        button.classList.add('calc__button');
+        button.textContent = btnValue;
+       
+        button.addEventListener('click', ()=> {
+            console.log(btnValue)
+            doClick(btnValue)});
+
         return button;
     }
+
+    // По саписку символов рисует клавитатуру
     function drawKeyboard (keys) {
+        
         const keyboard =  document.createElement('div');
+        
         keyboard.classList.add('calc__keyboard');
         for(i of keys){
             const button = drawCalcBtn(i);
             keyboard.append(button);
         }
-
+        
         return keyboard;
     }
-    function doCalculation (x, y, operator) {
+
+    // При нажатии на кнопку
+    function doClick (btnValue) {
+        
+        const operandsList = [ '%', '/' , 'x',  '+' ];
+    
+
+     
+
+        if(btnValue === 'ac') {
+            
+            calculatorExemplar.setX = 0;
+            calculatorExemplar.setY = 0;
+            
+            value1 = '';
+            value2 = '';
+            value =  '';
+            result = 0;
+            operator = '';
+            value1IsEmpty = true;
+
+        }
+
+        else if (btnValue === '⇐') {
+            
+            if(result) {
+                result = String(result).slice(0, -1);
+            }
+            
+            if(result) {
+                result = String(result).slice(0, -1);
+            }
+        }
+        // else if('%/x+'.includes(btnValue)) {
+        //     OperatorAction()
+        // }
+        // else if(btnValue === '%') {
+        //     OperatorAction()
+        // } else if(btnValue === '/') {
+        //     OperatorAction()
+        // } else if(btnValue === 'x') {
+        //     OperatorAction()
+        // } else if(btnValue === '+') {
+        //     OperatorAction()
+        // }
+ 
+        else if(isNaN(btnValue) && operandsList.includes(btnValue)) {
+            getRepeatResult(btnValue) 
+        } 
+        
+        else if(btnValue === '-' ) {
+            if( value1 === '') {
+                value = btnValue +  value;
+            }
+            if( operator && value2 === '') {
+                value = btnValue +  value;
+            }
+            else {
+                getRepeatResult(btnValue) 
+            }
+        } 
+        
+        else if(btnValue === '.') {
+            value = value+btnValue;
+        } 
+        
+        else if(btnValue === '=') {
+            getRepeatResult('', value)
+        } 
+        
+        else {
+            value = value + btnValue;
+        }
+        
+
+
+        if(value1IsEmpty) {
+            value1 = value;
+        }
+        else {
+            value2 = value;
+        }
+        
+        console.log(value1);
+        console.log(operator);
+        console.log(value2);
+        if(lastRender) {
+            Render(`${value1}${operator}${value2}=${result}`)
+            
+            lastRender = false;
+        }
+        else {
+            if(value1 === '' && value2 === '') {
+                Render(result)
+            }else {
+                Render(`${value1}${operator}${value2}`)
+            }
+            
+        }
+        
 
     }
+
+    // результат мат операции
+    function getResult() {
+       try {
+        console.log('va1', value1, 'va2', value2);
+        calculatorExemplar.setX(Number(value1));
+        calculatorExemplar.setY(Number(value2));
+        if(operator === '+') {
+            result = calculatorExemplar.logSum();
+        }
+        if(operator === '-') {
+            result = calculatorExemplar.logSub();
+        }
+        if(operator === '/') {
+            result = calculatorExemplar.logDiv();
+        }
+        if(operator === '%') {
+            result = calculatorExemplar.logDivCirc();
+        }
+        if(operator === 'x') {
+            result = calculatorExemplar.logMul();
+        }
+        
+        lastRender = true;
+       }
+       catch (err) {
+        Render('err')
+       } 
+
+     
+    }
+
+    function getRepeatResult(btnValue, val = '') { 
+        if(value1 && value2) { 
+            getResult();
+            value1 = result;
+            value2 = '';
+            lastRender = false;
+        } 
+        
+        value1IsEmpty = false;
+        operator = btnValue;
+        value = val;
  
-    window.addEventListener('DOMContentLoaded', ()=> {
-        const keys = ['ac', '<=', '%', '/', 7, 8,9,'x',4,5,6, '-',1,2,3,'+', 0, '.', '='];
+    }
 
-        const app = document.getElementById('app');
-        const display = drawDisplay(0);
+    // Отрисовка калькулятора
+    const Render = (displayText) => {
+        
         const keyboard = drawKeyboard(keys);
+        
+        app.innerHTML = '';
 
-        const calcFrame = drawCalcFrame(display, keyboard);
-
-
+        const display = drawDisplay(displayText);
+        const calcFrame = drawCalcFrame(keyboard, display);
+        
         app.append(calcFrame);
-    })
+        
+    }  
+
+    Render(0);
+
 })();
